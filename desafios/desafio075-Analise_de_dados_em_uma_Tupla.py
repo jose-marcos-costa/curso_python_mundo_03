1fcb2206f820786b22fca27e4158e2ec750e67bf
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-16 14:53:14 -0300

    Adicionando a resolução ao arquivo 'desafio075-Analise_de_dados_em_uma_Tupla.py' da 'aula 16'

--- desafios/desafio075-Analise_de_dados_em_uma_Tupla.py
+++ desafios/desafio075-Analise_de_dados_em_uma_Tupla.py
@@ -1,1 +1,16 @@
-# O arquivo './desafios/desafio075-Analise_de_dados_em_uma_Tupla.py' foi criado automaticamente
+num1 = int(input('Digite o 1º valor: '))
+num2 = int(input('Digite o 2º valor: '))
+num3 = int(input('Digite o 3º valor: '))
+num4 = int(input('Digite o 4º valor: '))
+digitados = (num1, num2, num3, num4)
+print(f'Você digitou os valores {digitados}')
+print(f'O número 9 apareceu {digitados.count(9)} vezes')
+try:
+    digitados.index(3)
+    print(f'O primeiro valor 3 foi digitado na {digitados.index(3) + 1}ª posição.')
+except ValueError:
+    print(f'O valor 3 não foi digitado em nenhuma posição!')
+print(f'Os valores pares digitados foram: ', end='')
+for n in digitados:
+    if n % 2 == 0:
+        print('{}'.format(n), end=' ')
