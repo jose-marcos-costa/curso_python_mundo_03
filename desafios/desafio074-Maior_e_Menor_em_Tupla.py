812c36d9e7829c2923280aebb7bbf2ec92a36bad
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-16 14:19:40 -0300

    Adicionando a resolução ao arquivo 'desafio074-Maior_e_Menor_em_Tupla.py' da 'aula 16'

--- desafios/desafio074-Maior_e_Menor_em_Tupla.py
+++ desafios/desafio074-Maior_e_Menor_em_Tupla.py
@@ -1,1 +1,11 @@
-# O arquivo './desafios/desafio074-Maior_e_Menor_em_Tupla.py' foi criado automaticamente
+from random import randint
+num1 = randint(0, 100)
+num2 = randint(0, 100)
+num3 = randint(0, 100)
+num4 = randint(0, 100)
+num5 = randint(0, 100)
+numeros = (num1, num2, num3, num4, num5)
+print(numeros)
+maior = sorted(numeros)[0]
+menor = sorted(numeros)[len(numeros)-1]
+print(f'O maior número na tupla é {maior} e o menor número na tupla é {menor}.')
