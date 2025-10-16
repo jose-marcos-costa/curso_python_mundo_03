7f321e952fc3c14fa4c50d728f771fc598e23af0
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-16 10:43:06 -0300

    Adicionando a resolução ao arquivo 'desafio072-Numero_por_Extenso.py' da 'aula 16'

--- desafios/desafio072-Numero_por_Extenso.py
+++ desafios/desafio072-Numero_por_Extenso.py
@@ -1,1 +1,12 @@
-# O arquivo './desafios/desafio072-Numero_por_Extenso.py' foi criado automaticamente
+numeros = (
+    'zero', 'um', 'dois', 'três', 'quatro',
+    'cinco', 'seis', 'sete', 'oito', 'nove',
+    'dez', 'onze', 'doze', 'treze', 'quatorze',
+    'quinze', 'dezesseis', 'dezessete', 'dezoito',
+    'dezenove', 'vinte'
+)
+teclado = int(input('Digite um número [0 a 20]: '))
+while teclado < 0 or teclado > 20:
+    print(f'Número Inválido!')
+    teclado = int(input('Digite um número [0 a 20]: '))
+print(numeros[teclado])
