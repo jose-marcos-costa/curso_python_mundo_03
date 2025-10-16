6fc871df297c9588f9fcf9fdc5c2040cad2da001
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-16 15:58:48 -0300

    Adicionando a resolução ao arquivo 'desafio077-Contando_Vogais_em_Tupla.py' da 'aula 16'

--- desafios/desafio077-Contando_Vogais_em_Tupla.py
+++ desafios/desafio077-Contando_Vogais_em_Tupla.py
@@ -1,1 +1,19 @@
-# O arquivo './desafios/desafio077-Contando_Vogais_em_Tupla.py' foi criado automaticamente
+palavras = (
+    'AVIAO',
+    'CASA',
+    'FUTEBOL',
+    'MERCADO',
+    'JANELA',
+    'ESCOLA',
+    'FLORESTA',
+    'GATO',
+    'ESTACAO',
+    'VIAGEM'
+)
+for c in palavras:
+    print(f'Na palavra {c} temos ', end=' ')
+    for letra in c.lower():
+        if letra in 'aeiou':
+            print(f'{letra}', end=' ')
+    print('')
+
