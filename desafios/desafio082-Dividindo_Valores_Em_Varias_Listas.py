69c1fa4bc04667b3bf29a6ba104e66efac4290e4
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-17 17:40:55 -0300

    Adicionando a resolução ao arquivo 'desafio082-Dividindo_Valores_Em_Varias_Listas.py' da 'aula 17'

--- desafios/desafio082-Dividindo_Valores_Em_Varias_Listas.py
+++ desafios/desafio082-Dividindo_Valores_Em_Varias_Listas.py
@@ -1,1 +1,20 @@
-# O arquivo './desafios/desafio082-Dividindo_Valores_Em_Varias_Listas.py' foi criado automaticamente
+numeros = []
+while True:
+    numeros.append(int(input('Digite um valor: ')))
+    while True:
+        opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+        if opcao in 'NS':
+            break
+    if opcao == 'N':
+        break
+impares = []
+pares = []
+for n in numeros:
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        impares.append(n)
+print('-=' * 30)
+print(f'A lista completa é {numeros}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de ímpares é {impares}')
