46107f3d8055bc51b011a06201c55b3376fc2c4d
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-17 17:19:22 -0300

    Adicionando a resolução ao arquivo 'desafio081-Extraindo_Dados_De_Uma_Lista.py' da 'aula 17'

--- desafios/desafio081-Extraindo_Dados_De_Uma_Lista.py
+++ desafios/desafio081-Extraindo_Dados_De_Uma_Lista.py
@@ -1,1 +1,16 @@
-# O arquivo './desafios/desafio081-Extraindo_Dados_De_Uma_Lista.py' foi criado automaticamente
+valores = []
+opcao = 'S'
+while opcao == 'S':
+    valores.append(int(input('Digite um valor: ')))
+    while True:
+        opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+        if opcao in 'SN':
+            break
+print('-=' * 30)
+print(f'Você digitou {len(valores)} elementos')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print(f'O valor 5 faz parte da lista!')
+else:
+    print(f'O valor 5 não foi encontrado na lista!')
