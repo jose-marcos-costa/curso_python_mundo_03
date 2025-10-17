38c66ede8bd2a01df22955ff26fd4667607b6c26
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-17 18:03:43 -0300

    Adicionando a resolução ao arquivo 'desafio083-Validando_Expressoes_Matematicas.py' da 'aula 17'

--- desafios/desafio083-Validando_Expressoes_Matematicas.py
+++ desafios/desafio083-Validando_Expressoes_Matematicas.py
@@ -1,1 +1,17 @@
-# O arquivo './desafios/desafio083-Validando_Expressoes_Matematicas.py' foi criado automaticamente
+expressao = []
+expressao.append(str(input('Digite a expressão: ')).strip())
+cont_abre = cont_fecha = 0
+print(f'{expressao[0]}')
+for v in expressao[0]:
+    if v == '(':
+        cont_abre += 1
+    elif v == ')':
+        cont_fecha += 1
+# print(f'Abriu {cont_abre} vezes')
+# print(f'Fechou {cont_fecha} vezes')
+if cont_abre == cont_fecha:
+    print(f'Sua expressão está certa!')
+else:
+    print(f'Sua expressão está errada!')
+
+
