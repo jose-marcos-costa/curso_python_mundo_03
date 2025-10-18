53e178c08183b1fce97f00c7b5cc9016461d0d5d
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-18 16:51:11 -0300

    Adicionando a resoluçãpo ao arquivo 'desafio086-Matriz_Em_Python.py' da 'aula 18'

--- desafios/desafio086-Matriz_Em_Python.py
+++ desafios/desafio086-Matriz_Em_Python.py
@@ -1,1 +1,10 @@
-# O arquivo './desafios/desafio086-Matriz_Em_Python.py' foi criado automaticamente
+lista = [[],[],[]]
+for i in range(0, 3):
+    for j in range(0, 3):
+        lista[j].append(int(input(f'Digite um valor para [{i}, {j}]: ')))
+# print(lista)
+print('-=' * 40)
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(f'[ {lista[j][i]} ]', end='')
+    print('')
