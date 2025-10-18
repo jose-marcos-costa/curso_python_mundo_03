bfbfedaafcf14dc8bac33cda9164b9850a521723
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-18 20:25:46 -0300

    Adicionando a resolução ao arquivo 'desafio088-Palpites_Para_A_Mega_Sena.py' da 'aula 18'

--- desafios/desafio088-Palpites_Para_A_Mega_Sena.py
+++ desafios/desafio088-Palpites_Para_A_Mega_Sena.py
@@ -1,1 +1,24 @@
-# O arquivo './desafios/desafio088-Palpites_Para_A_Mega_Sena.py' foi criado automaticamente
+from time import sleep
+from random import randint
+jogos = list()
+dados = list()
+print('-' * 40)
+print('{:^40}'.format('JOGO DA MEGA SENA'))
+print('-' * 40)
+vezes = int(input(f'Quantos jogos você quer que eu sorteie? '))
+print(f'SORTEANDO {vezes} JOGOS')
+for i in range(0, vezes):
+    while len(dados) < 6:
+        num = randint(1, 60)
+        if num in dados:
+            # print('Já tem...')
+            continue
+        else:
+            dados.append(num)
+    # print(dados)
+    dados.sort()
+    print(dados)
+    jogos.append(dados[:])
+    dados.clear()
+    sleep(1)
+print('{:=^40}'.format(' < BOA SORTE! >'))
