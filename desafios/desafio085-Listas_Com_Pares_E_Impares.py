2f69f57c3963ef6522aafbd0e0c584763152ccf4
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-18 16:31:33 -0300

    Adicionando a resoluçãpo ao arquivo 'desafio085-Listas_Com_Pares_E_Impares.py' da 'aula 18'

--- desafios/desafio085-Listas_Com_Pares_E_Impares.py
+++ desafios/desafio085-Listas_Com_Pares_E_Impares.py
@@ -1,1 +1,14 @@
-# O arquivo './desafios/desafio085-Listas_Com_Pares_E_Impares.py' foi criado automaticamente
+lista = [[],[]]
+# print(lista)
+for i in range(1, 8):
+    num = int(input(f'Digite o {i}º valor: '))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+print('-=' * 30)
+# print(lista)
+lista[0].sort()
+lista[1].sort()
+print(f'Os valores pares digitados foram: {lista[0]}')
+print(f'Os valores ímpares digitados foram: {lista[1]}')
