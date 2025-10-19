fdbac950a02f50ab05d3a40d15581cd7471073e2
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-19 07:34:37 -0300

    Corrigindo a resolução do 'desafio084-Lista_Composta_E_Analise_De_Dados.py' da 'aula 18'

--- desafios/desafio084-Lista_Composta_E_Analise_De_Dados.py
+++ desafios/desafio084-Lista_Composta_E_Analise_De_Dados.py
@@ -1,12 +1,18 @@
 geral = list()
 dados = list()
-auxiliar = list()
 mais_pesadas = list()
 mais_leves = list()
 maior = menor = 0
 while True:
     dados.append(str(input('Nome: ')))
     dados.append(float(input('Peso (Kg): ')))
+    if len(geral) == 0:
+        maior = menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]
     geral.append(dados[:]) # Cópia!!!
     dados.clear()
     while True:
@@ -16,17 +22,6 @@ while True:
     if opcao == 'N':
         break
 print(f'Foram cadastradas {len(geral)} pessoa(s)')
-for pos, valor in enumerate(geral):
-    if pos == 0:
-        maior = valor[1]
-        menor = valor[1]
-    else:
-        if valor[1] > maior:
-            maior = valor[1]
-        elif valor[1] < menor:
-            menor = valor[1]
-# print(maior)
-# print(menor)
 for v in geral:
     if v[1] == maior:
         mais_pesadas.append(v[0])
