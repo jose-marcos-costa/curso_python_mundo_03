ca4e7b2ac5f1f0fae318eb0bc73bcc54ad1fc8c7
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-19 08:24:38 -0300

    Corrigindo a resolução do 'desafio086-Matriz_Em_Python.py' da 'aula 18'

--- desafios/desafio086-Matriz_Em_Python.py
+++ desafios/desafio086-Matriz_Em_Python.py
@@ -1,10 +1,10 @@
 lista = [[],[],[]]
 for i in range(0, 3):
     for j in range(0, 3):
-        lista[j].append(int(input(f'Digite um valor para [{i}, {j}]: ')))
+        lista[i].append(int(input(f'Digite um valor para [{i}, {j}]: ')))
 # print(lista)
 print('-=' * 40)
 for i in range(0, 3):
     for j in range(0, 3):
-        print(f'[ {lista[j][i]} ]', end='')
-    print('')
+        print(f'[ {lista[i][j]:^5} ]', end='')
+    print()
