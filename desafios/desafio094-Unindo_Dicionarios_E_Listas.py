e801a563f69ba420090983c336959d8eb04d2cce
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-21 19:52:03 -0300

    Adicionando a resolução ao arquivo 'desafio094-Unindo_Dicionarios_E_Listas.py' da 'aula 19'

--- desafios/desafio094-Unindo_Dicionarios_E_Listas.py
+++ desafios/desafio094-Unindo_Dicionarios_E_Listas.py
@@ -1,1 +1,36 @@
-# O arquivo './desafios/desafio094-Unindo_Dicionarios_E_Listas.py' foi criado automaticamente
+pessoa = dict()
+grupo = list()
+soma_idade = media_idade = 0
+cria_nome = ''
+cont = 0
+while True:
+    pessoa['nome'] = str(input('Nome: ')).strip()
+    pessoa['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    pessoa['idade'] = int(input('Idade: '))
+    grupo.append(pessoa.copy())
+    pessoa.clear()
+    while True:
+        opcao = str(input('Deseja continuar [S/N]: ')).strip().upper()[0]
+        if opcao in 'SN':
+            break
+    if opcao == 'N':
+        break
+print('-=' * 30)
+print(f'- O grupo tem {len(grupo)}.')
+print(f'- As mulheres cadastradas foram: ', end='')
+for i, p in enumerate(grupo):
+    soma_idade += grupo[i]['idade']
+    if grupo[i]['sexo'] == 'F':
+        print(grupo[i]['nome'],end=' ')
+print()
+media_idade = soma_idade / len(grupo)
+print(f'- A média de idade é {media_idade}')
+print(f'- Lista de pessoas que estão acima da média:')
+for p in grupo:
+    print()
+    if p['idade'] > media_idade:
+        for k, v in p.items():
+            print(f' {k} = {v}', end=';')
+print()
+print('-=' * 30)
+print('<<< ENCERRADO >>>')
