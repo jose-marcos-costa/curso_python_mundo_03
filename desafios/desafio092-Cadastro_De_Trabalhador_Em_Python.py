11106aadf9be52adcd13d31c404a52e680b18c4f
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-21 16:45:26 -0300

    Adicionando a resolução ao arquivo 'desafio092-Cadastro_De_Trabalhador_Em_Python.py' da 'aula 19'

--- desafios/desafio092-Cadastro_De_Trabalhador_Em_Python.py
+++ desafios/desafio092-Cadastro_De_Trabalhador_Em_Python.py
@@ -1,1 +1,12 @@
-# O arquivo './desafios/desafio092-Cadastro_De_Trabalhador_Em_Python.py' foi criado automaticamente
+from datetime import date
+trabalhador = dict()
+trabalhador['nome'] = str(input('Nome: '))
+ano_nascimento = int(input('Ano de Nascimento: '))
+trabalhador['idade'] = date.today().year - ano_nascimento
+trabalhador['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if trabalhador['ctps'] != 0:
+    trabalhador['contratação'] = int(input('Ano de contratação: '))
+    trabalhador['salario'] = float(input('Salário: '))
+    trabalhador['aposentadoria'] = (trabalhador['contratação']  - ano_nascimento) + 35
+for k, v in trabalhador.items():
+    print(f'{k} tem valor {v}')
