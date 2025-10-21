b9b8fc2b1885a782ff0eceb96b9e7efbfed5e4bd
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-21 15:24:49 -0300

    Adicionando a resolução ao arquivo 'desafio090-Dicionario_Em_Python.py' da 'aula 19'

--- desafios/desafio090-Dicionario_Em_Python.py
+++ desafios/desafio090-Dicionario_Em_Python.py
@@ -1,1 +1,10 @@
-# O arquivo './desafios/desafio090-Dicionario_Em_Python.py' foi criado automaticamente
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['media'] >= 7.0:
+    aluno['situacao'] = 'Aprovado'
+else:
+    aluno['situacao'] = 'Reprovado'
+print(f'Nome é igual a {aluno["nome"]}')
+print(f'Média é igual a {aluno["media"]}')
+print(f'Situação é iugal {aluno["situacao"]}')
