5e8cfdd87b73db000aead580b11b4ce2d71cf299
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-22 11:07:01 -0300

    Adicionando a resolução ao arquivo 'desafio096-Funcao_Que_Calcula_Area.py' da 'aula 20'

--- desafios/desafio096-Funcao_Que_Calcula_Area.py
+++ desafios/desafio096-Funcao_Que_Calcula_Area.py
@@ -1,1 +1,12 @@
-# O arquivo './desafios/desafio096-Funcao_Que_Calcula_Area.py' foi criado automaticamente
+def area(l, c):
+    a = l * c
+    print(f'A área de um terreno {l:.1f}x{c:.1f} é de {a:.1f}m².')
+
+
+# Programa Principal
+print('-' * 30)
+print(f'{"Controle de Terrenos":^30}')
+print('-' * 30)
+largura = float(input('LARGURA (m): '))
+comprimento = float(input('COMPRIMENTO (m): '))
+area(largura, comprimento)
