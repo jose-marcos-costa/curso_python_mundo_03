12604d252edda0ab2aa8e0e4a997d9c2067d0002
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-22 14:26:25 -0300

    Adicionando a resolução ao arquivo 'desafio100-Funcoes_Para_Sortear_E_Somar.py' da 'aula 20'

--- desafios/desafio100-Funcoes_Para_Sortear_E_Somar.py
+++ desafios/desafio100-Funcoes_Para_Sortear_E_Somar.py
@@ -1,1 +1,25 @@
-# O arquivo './desafios/desafio100-Funcoes_Para_Sortear_E_Somar.py' foi criado automaticamente
+from random import randint
+numeros = list()
+
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for i in range(0, 5):
+        lista.append(randint(1, 10))
+        print(f'{lista[i]} ', end= '')
+    print('PRONTO!')
+
+
+def somaPar(lst):
+    soma = 0
+    for v in lst:
+        if v % 2 == 0:
+            soma += v
+    print(f'Somando os valores pares de {numeros}, temos {soma}.')
+
+
+# Programa Principal
+sorteia(numeros)
+# print(numeros)
+somaPar(numeros)
+
