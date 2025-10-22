3f63094eb658d037222e4cebc6cde75e8c5a1a43
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-21 21:40:34 -0300

    Corrigindo a resolução do 'desafio090-Dicionario_Em_Python.py' da 'aula 19'

--- desafios/desafio090-Dicionario_Em_Python.py
+++ desafios/desafio090-Dicionario_Em_Python.py
@@ -1,10 +1,12 @@
 aluno = dict()
 aluno['nome'] = str(input('Nome: '))
 aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
-if aluno['media'] >= 7.0:
+if aluno['media'] >= 7:
     aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situacao'] = 'Recuperação'
 else:
     aluno['situacao'] = 'Reprovado'
-print(f'Nome é igual a {aluno["nome"]}')
-print(f'Média é igual a {aluno["media"]}')
-print(f'Situação é iugal {aluno["situacao"]}')
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
