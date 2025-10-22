e743412040c14f6798f656d35c8b0f822df38586
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-22 14:53:41 -0300

    Corrigindo a resolução do 'desafio098-Funcao_De_Contador.py' da 'aula 20'

--- desafios/desafio098-Funcao_De_Contador.py
+++ desafios/desafio098-Funcao_De_Contador.py
@@ -3,13 +3,13 @@ def contador(inicio, fim, passo):
     print('-=' * 30)
     print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
     if inicio > fim and passo != 0 :
-        for i in range(inicio, (fim - passo), -passo):
-            print(f'{i}', end=' -> ')
+        for cont in range(inicio, (fim - passo), -passo):
+            print(f'{cont}', end=' -> ')
             sleep(0.5)
         print('FIM!')
     if (fim > inicio and passo != 0) or passo < 0:
-        for i in range(inicio, (fim + passo), passo):
-            print(f'{i}', end=' -> ')
+        for cont in range(inicio, (fim + passo), passo):
+            print(f'{cont}', end=' -> ')
             sleep(0.5)
         print('FIM!')
     if passo == 0:
@@ -17,17 +17,18 @@ def contador(inicio, fim, passo):
             passo = -1
         elif fim > inicio:
             passo = 1
-        for i in range(inicio, (fim + passo), passo):
-            print(f'{i}', end=' -> ')
+        for cont in range(inicio, (fim + passo), passo):
+            print(f'{cont}', end=' -> ')
             sleep(0.5)
         print('FIM!')
 
 # Programa Principal
 contador(1, 10, 1)
 contador(10, 0, 2)
-i = int(input(f'Digite o início: '))
-f = int(input(f'Digite o fim: '))
-p = int(input(f'Digite o passo: '))
-contador(i, f, p)
+print('Agora é sua vez de personalizar a contagem!')
+ini = int(input(f'Digite o início: '))
+fim = int(input(f'Digite o fim: '))
+pas = int(input(f'Digite o passo: '))
+contador(ini, fim, pas)
 
 
