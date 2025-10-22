09e62ce71c6281b8719d7b9e36bfe42cc8fca1f6
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-21 21:59:15 -0300

    Corrigindo a resolução do 'desafio091-Jogo_De_Dados_Em_Python.py' da 'aula 19'

--- desafios/desafio091-Jogo_De_Dados_Em_Python.py
+++ desafios/desafio091-Jogo_De_Dados_Em_Python.py
@@ -1,17 +1,15 @@
 from random import randint
 from time import sleep
-jogadores = {'jogador1': 0,
-             'jogador2': 0,
-             'jogador3': 0,
-             'jogador4': 0}
+from operator import itemgetter
+jogadores = {'jogador1': randint(1, 6),
+             'jogador2': randint(1, 6),
+             'jogador3': randint(1, 6),
+             'jogador4': randint(1, 6)}
 print('Valores sorteados:')
-for i in range (1, 5):
-    chave = 'jogador' + str(i)
-    jogadores[chave] = randint(1, 6)
-    print(f'\t{chave} = {jogadores[chave]}')
+for k, v in jogadores.items():
+    print(f'\t{k} tirou {v} no dado')
     sleep(0.5)
-
-ordenado = dict(sorted(jogadores.items(), key=lambda item: item[1], reverse=True))
+ordenado = dict(sorted(jogadores.items(), key=itemgetter(1), reverse=True))
 print('-=' * 30)
 print('Ranking dos jogadores:')
 cont = 1
