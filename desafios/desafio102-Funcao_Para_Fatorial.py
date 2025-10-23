4c308339743d8846479409e772747711e407da84
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-23 10:57:36 -0300

    Adicionando a resolução ao arquivo 'desafio102-Funcao_Para_Fatorial.py' da 'aula 21'

--- desafios/desafio102-Funcao_Para_Fatorial.py
+++ desafios/desafio102-Funcao_Para_Fatorial.py
@@ -1,1 +1,26 @@
-# O arquivo './desafios/desafio102-Funcao_Para_Fatorial.py' foi criado automaticamente
+def fatorial(num, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param num: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    f = 1
+    if show == False:
+        for c in range(num, 0, -1):
+            f *= c
+    else:
+        for c in range(num, 0, -1):
+            f *= c
+            if c == 1:
+                print(f'{c}', end=' ')
+            else:
+                print(f'{c}', end=' x ')
+        print(f'= ', end='')
+    return f
+
+
+# Programa Principal
+print(fatorial(5))
+print('-' * 40)
+help(fatorial)
