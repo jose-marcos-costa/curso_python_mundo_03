5f815c8a2a7eec6834a4f052f03e0fd7f804ab97
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-24 10:30:17 -0300

    Adicionando a resolução ao arquivo 'desafio106-Interactive_Helping_System_In_Python.py' da 'aula 21'

--- desafios/desafio106-Interactive_Helping_System_In_Python.py
+++ desafios/desafio106-Interactive_Helping_System_In_Python.py
@@ -1,1 +1,26 @@
-# O arquivo './desafios/desafio106-Interactive_Helping_System_In_Python.py' foi criado automaticamente
+def pyHelp():
+    while True:
+        imprimeColorido('SISTEMA DE AJUDA PyHELP', cores['amarelo_back'])
+        opcao = str(input('Função ou Biblioteca> ')).strip()
+        if opcao == 'SAIR':
+            imprimeColorido('ATÉ LOGO!', cores['vermelho_back'])
+            break
+        frase = "Acessando o manual do comando/biblioteca '" + opcao + "'"
+        imprimeColorido(frase, cores['azul_back'])
+        help(opcao)
+
+def imprimeColorido(f, cor):
+    tam = len(f)+4
+    print('{}'.format(cor), '~' * tam )
+    print(f'   {f}')
+    print('{}'.format(cor), '~' * tam )
+    print('{}'.format(cores['limpa']))
+
+
+# Programa Principal
+cores = { 'amarelo_back': '\33[30;43m',
+          'limpa': '\33[m',
+          'azul_back': '\33[30;44m',
+          'vermelho_back': '\33[30;41m',
+          }
+pyHelp()
