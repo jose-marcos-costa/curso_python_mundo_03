fd07c30d6cc8b5ed921ca77ddbeab734c16dfbf5
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-24 09:24:38 -0300

    Adicionando a resolução ao arquivo 'desafio104-Validando_Entrada_De_Dados_Em_Python.py' da 'aula 21'

--- desafios/desafio104-Validando_Entrada_De_Dados_Em_Python.py
+++ desafios/desafio104-Validando_Entrada_De_Dados_Em_Python.py
@@ -1,1 +1,11 @@
-# O arquivo './desafios/desafio104-Validando_Entrada_De_Dados_Em_Python.py' foi criado automaticamente
+def leiaInt(frase):
+    resp = input(f'{frase}')
+    while not resp.isnumeric():
+        print('{}ERRO! Digite um número inteiro válido.{}'.format('\33[31m', '\33[m'))
+        resp = input(f'{frase}')
+    return resp
+
+
+# Programa Principal
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
