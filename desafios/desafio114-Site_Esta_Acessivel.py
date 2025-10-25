61918fb716eebf7668881ae9ee69f7596ef31220
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-25 12:55:56 -0300

    Adicionando a resolução ao arquivo 'desafio114-Site_Esta_Acessivel.py' da 'aula 23'

--- desafios/desafio114-Site_Esta_Acessivel.py
+++ desafios/desafio114-Site_Esta_Acessivel.py
@@ -1,1 +1,12 @@
-# O arquivo './desafios/desafio114-Site_Esta_Acessivel.py' foi criado automaticamente
+import requests
+from requests.exceptions import HTTPError
+
+url = "https://www.pudim.com.br/"
+try:
+    resp = requests.get(url)
+    resp.raise_for_status()
+except Exception:
+    print(f'O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso!')
+
