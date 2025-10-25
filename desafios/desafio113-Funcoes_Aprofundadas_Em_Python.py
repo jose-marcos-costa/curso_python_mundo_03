13487eafb3cc083de4b4de59d63fb672043dd8aa
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-25 11:02:20 -0300

    Adicionando a resolução ao arquivo 'desafio113-Funcoes_Aprofundadas_Em_Python.py' da 'aula 23'

--- desafios/desafio113-Funcoes_Aprofundadas_Em_Python.py
+++ desafios/desafio113-Funcoes_Aprofundadas_Em_Python.py
@@ -1,1 +1,15 @@
-# O arquivo './desafios/desafio113-Funcoes_Aprofundadas_Em_Python.py' foi criado automaticamente
+def leiaInt(frase):
+    try:
+        resp = int(input(f'{frase}'))
+    except Exception as erro:
+        print('{}ERRO {}: por favor, digite um número inteiro válido.{}'.format('\033[31m', erro.__class__, '\033[m'))
+    except KeyboardInterrupt:
+        print('Usuário preferiu não digitar esse número.')
+    else:
+        return resp
+
+# Programa Principal
+ni = leiaInt('Digite um Inteiro: ')
+#nr = leiaReal('Digite um Real: ')
+print(f'O valor interiro digitado foi {ni}')
+# e o real foi {nr}')
