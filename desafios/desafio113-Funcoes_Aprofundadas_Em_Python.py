c9f37f41faf223e9e11c13f29a3ffd3735308e4c
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-25 11:30:42 -0300

    Corrigindo o 'desafio113-Funcoes_Aprofundadas_Em_Python.py' da 'aula 23'

--- desafios/desafio113-Funcoes_Aprofundadas_Em_Python.py
+++ desafios/desafio113-Funcoes_Aprofundadas_Em_Python.py
@@ -1,15 +1,32 @@
-def leiaInt(frase):
-    try:
-        resp = int(input(f'{frase}'))
-    except Exception as erro:
-        print('{}ERRO {}: por favor, digite um número inteiro válido.{}'.format('\033[31m', erro.__class__, '\033[m'))
-    except KeyboardInterrupt:
-        print('Usuário preferiu não digitar esse número.')
-    else:
-        return resp
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(f'{msg}'))
+        except (ValueError, TypeError):
+            print('{}ERRO: por favor, digite um número interiro válido.{}'.format('\033[31m', '\033[m'))
+            continue
+        except KeyboardInterrupt:
+            print('\n{}Usuário preferiu não digitar esse número.{}'.format('\033[31m', '\033[m'))
+            return 0
+        else:
+            return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(f'{msg}'))
+        except (ValueError, TypeError):
+            print('{}ERRO: por favor, digite um número real válido.{}'.format('\033[31m', '\033[m'))
+            continue
+        except KeyboardInterrupt:
+            print('\n{}Usuário preferiu não digitar esse número.{}'.format('\033[31m', '\033[m'))
+            return 0
+        else:
+            return n
+
 
 # Programa Principal
 ni = leiaInt('Digite um Inteiro: ')
-#nr = leiaReal('Digite um Real: ')
-print(f'O valor interiro digitado foi {ni}')
-# e o real foi {nr}')
+nr = leiaFloat('Digite um Real: ')
+print(f'O valor inteiro digitado foi {ni} e o real foi {nr}')
